6287fc73932d84f74407ac2523cd8adf234f68a2
simoare <lellothegreat@protonmail.ch>
2022-04-15 19:40:15 +0200

    finalized interface

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import tkinter as ttk
 from tkinter import *
 from tkinter import messagebox
 from tkinter.simpledialog import askstring
+from PIL import Image, ImageTk
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
         self.title('The Crypt')
+        self.image = Image.open('thecrypt.jpg')
+        self.photo = ImageTk.PhotoImage(self.image)
         self.create_widgets()
 
     def encryption_magic(self):
@@ -16,41 +19,69 @@ class App(tk.Tk):
         print(disposal)
         
     def create_widgets(self):
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
         self.v1 = tk.IntVar()
         self.alpha_check = tk.IntVar()
         self.special_check = tk.IntVar()
 
+        # Create an image Label
+        self.img_label = ttk.Label(image=self.photo, anchor=CENTER)
+        self.img_label.image = self.photo
+
+        self.img_label.grid(row=0, column=1)  
+
         # label
-        ttk.Label(text='User:').grid(row=0, column=0, padx=5, pady=5)
-        ttk.Label(text='Passwd:').grid(row=1, column=0, padx=5, pady=5)
-        ttk.Label(text='Lenght').grid(row=3, column=0, padx=5, pady=5)
-        ttk.Label(text='A-aZ-z').grid(row=4, column=0, padx=5, pady=5)
-        ttk.Label(text='@#!*').grid(row=5, column=0, padx=5, pady=5)
+        ttk.Label(text='User:').grid(row=10, column=0, sticky='w')
+        ttk.Label(text='Passwd: ').grid(row=11, column=0, sticky='w')
+        tk.Label(text='URL:').grid(row=12, column=0, sticky='w')
+        ttk.Label(text='User:').grid(row=14, column=0, sticky='w')
+        ttk.Label(text='TheCrypt:').grid(row=15, column=0, sticky='w')
+        ttk.Label(text='URL:').grid(row=16, column=0, sticky='w')
+        ttk.Label(text='Lenght:').grid(row=17, column=0, sticky='w')
+        ttk.Label(text='A-aZ-z').grid(row=18, column=0, sticky='w')
+        ttk.Label(text='@#!*').grid(row=19, column=0, sticky='w')
 
         # User Entry
-        self.user_entry = ttk.Entry(self, width=40)
-        self.user_entry.grid(row=0, column=1, columnspan=2, padx=5)
+        self.user_entry = ttk.Entry(self, width=30)
+        self.user_entry.grid(row=10, column=1, columnspan=2, padx=10, pady=10)
 
         # Password Entry
-        self.passwd_entry = ttk.Entry(self, width=40)
-        self.passwd_entry.grid(row=1, column=1, columnspan=2, padx=5)
+        self.passwd_entry = ttk.Entry(self, width=30)
+        self.passwd_entry.grid(row=11, column=1, columnspan=2, padx=10, pady=10)
+
+        # URL Entry
+        self.url_entry = ttk.Entry(self, width=30)
+        self.url_entry.grid(row=12, column=1, columnspan=2, padx=10, pady=10)
+
+        # Second User
+        self.user_two = ttk.Entry(self, width=30)
+        self.user_two.grid(row=14, column=1, columnspan=2, padx=10, pady=10)
+
+        # TheCrypt
+        self.the_crypt = ttk.Entry(self, width=30, state=DISABLED)
+        self.the_crypt.grid(row=15, column=1, columnspan=2, padx=10, pady=10)
+
+        # Second URL
+        self.url_two = ttk.Entry(self, width=30)
+        self.url_two.grid(row=16, column=1, columnspan=2, padx=10, pady=10)
+
 
         # Slider Entry
         self.slide_entry = ttk.Scale(self, variable=self.v1, from_=1, to=32, orient=HORIZONTAL)
-        self.slide_entry.grid(row=3, column=1)
+        self.slide_entry.grid(row=17, column=1)
 
-        # Checknutton
+        # # Checknutton
         self.alpha_entry = ttk.Checkbutton(self, variable=self.alpha_check, text='')
-        self.alpha_entry.grid(row=4, column=1)
+        self.alpha_entry.grid(row=18, column=1)
 
-        # Checkbutton
+        # # Checkbutton
         self.special_entry = ttk.Checkbutton(self, variable=self.special_check, text='')
-        self.special_entry.grid(row=5, column=1)
+        self.special_entry.grid(row=19, column=1)
 
-        # button
-        self.send_button = ttk.Button(text='Add', command=self.validate_data).grid(row=0, column=4, padx=5)
+        # First Add Button
+        self.send_button = ttk.Button(text='Add Custom', command=self.validate_data).grid(row=13, padx=5, pady=5, column=1, sticky='s')
+
+        # Second Add Button
+        self.send_button = ttk.Button(text='The Crypt', command=self.validate_data).grid(row=20, padx=5, pady=5, column=1, sticky='s')
 
     def askMe(self, slide):
         res = messagebox.askquestion('askquestion', f'Password is {slide} chars long, are you ok?')
